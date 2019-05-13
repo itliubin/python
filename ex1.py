9a0b412578e1637b445f53aca13f78db3c4ee6c7
itliubin <itliubin@163.com>
2019-05-13 17:02:20 +0800

    add ex1.py file

---
+++ ex1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# encoding:utf-8
+'''
+@author:ben
+@contact:itliubin@163.com
+@file:ex1.py
+@date:2019/4/24_13:16
+
+'''
+
+print "hello world"
+print "hello again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+
