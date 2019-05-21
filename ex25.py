487f0014e5efeac3851ced5a2de29f9ab8e15119
itliubin <itliubin@163.com>
2019-05-21 17:08:55 +0800

    xiug ex25.py

--- ex25.py
+++ ex25.py
@@ -36,7 +36,23 @@ def print_last_word(words):
 
 def sort_sentence(sentence):
     """Takes in a full sentence and returns the sorted words."""
-    words = break_words()
+    words = break_words(sentence)
+    return sort_words(words)
+
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one"""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
 
 
 
