959af574b9739d164cea3262820a435044348cc8
itliubin <itliubin@163.com>
2019-05-21 17:20:09 +0800

    xiug ex25.py

--- ex25.py
+++ ex25.py
@@ -8,9 +8,6 @@
 
 '''
 
-from sys import  argv
-script, filename = argv
-
 
 def break_words(stuff):
     """ This function will break up words for us."""
@@ -27,6 +24,7 @@ def print_first_word(words):
     """Prints the first word after popping it off."""
     word = words.pop(0)
     print word
+
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
@@ -52,7 +50,3 @@ def print_first_and_last_sorted(sentence):
     words = sort_sentence(sentence)
     print_first_word(words)
     print_last_word(words)
-
-
-
-
