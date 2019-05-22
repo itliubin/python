62b7f21f27ccf26360eba7b2a10bd98f791a7167
itliubin <itliubin@163.com>
2019-05-22 17:07:59 +0800

    布尔表达式练习

---
+++ ex28.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# encoding:utf-8
+print "hello word"
+print True and True
+print False and True
+print 1 == 1 and 2 == 2
+print "test" == "test"
+print 1 == 1 and 2 != 1
+print True and 1 == 1
+print "test" == "testing"
